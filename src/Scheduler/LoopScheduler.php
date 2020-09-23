d5232233b30589d0f5b67e64411d5a258b01e37b
titrxw <1120309488@qq.com>
2020-09-23 10:13:19 +0800

    删除非必须操作

--- src/Scheduler/LoopScheduler.php
+++ src/Scheduler/LoopScheduler.php
@@ -1,11 +1,21 @@
 <?php
 
+/**
+ * Rangine crontab server
+ *
+ * (c) We7Team 2019 <https://www.rangine.com>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com for more details
+ */
+
 namespace W7\Crontab\Scheduler;
 
 use W7\Crontab\Task\Task;
 
 class LoopScheduler extends SchedulerAbstract {
-	public function schedule(){
+	public function schedule() {
 		itimeTick(1000, function () {
 			$time = time();
 			$tasks = $this->taskManager->all();
@@ -15,7 +25,6 @@ class LoopScheduler extends SchedulerAbstract {
 			 */
 			foreach ($tasks as $task) {
 				if ($task->check($time)) {
-					$tasks[$task->getName()] = $task;
 					$this->scheduleTask($task);
 				}
 			}
